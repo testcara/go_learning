20c44b795ecf953a93d4c07f00cc781f47cf9785
testcara <wlin@redhat.com>
2024-08-09 05:22:52 +0000

    add nake return function

--- functions/begin/main.go
+++ functions/begin/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func greet() string {
 	return "Hello World!"
@@ -10,7 +13,13 @@ func greetWithName(name string) string{
 	return "Hello, " + name + "!"
 }
 
+func greetWithNameAndAge(name string, age int) (greetings string){
+	greetings = "Hello, My name is " + name + ", and I am " + strconv.Itoa(age) + " years old"
+	return
+}
+
 func main () {
 	fmt.Println(greet())
 	fmt.Println(greetWithName("Cara"))
+	fmt.Println(greetWithNameAndAge("Cara", 35))
 }
