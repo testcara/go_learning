93e7cd0ad256068dbf992a6d19b3f0607b43c4be
testcara <wlin@redhat.com>
2024-08-09 05:34:12 +0000

    mutiple return values

--- functions/begin/main.go
+++ functions/begin/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 )
@@ -9,17 +10,26 @@ func greet() string {
 	return "Hello World!"
 }
 
-func greetWithName(name string) string{
+func greetWithName(name string) string {
 	return "Hello, " + name + "!"
 }
 
-func greetWithNameAndAge(name string, age int) (greetings string){
+func greetWithNameAndAge(name string, age int) (greetings string) {
 	greetings = "Hello, My name is " + name + ", and I am " + strconv.Itoa(age) + " years old"
 	return
 }
 
-func main () {
-	fmt.Println(greet())
-	fmt.Println(greetWithName("Cara"))
-	fmt.Println(greetWithNameAndAge("Cara", 35))
+func divid(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("cannot be divided by 0!")
+	}
+	return a / b, nil
+}
+
+func main() {
+	// fmt.Println(greet())
+	// fmt.Println(greetWithName("Cara"))
+	// fmt.Println(greetWithNameAndAge("Cara", 35))
+	fmt.Println(divid(2,1))
+	fmt.Println(divid(2,0))
 }
